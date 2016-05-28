02ade690519bcaff15aa6dd38eec725dd59ce769
joseph dias <josephdias92@gmail.com>
2016-05-28 21:02:39 +0530

    added auth interceptor to add bearer tokens

--- app/authentication/authentication.interceptor.js
+++ app/authentication/authentication.interceptor.js
@@ -1,3 +1,33 @@
-(function() {
-
-})();
+(function(module) {
+  'use strict';
+  module.factory('authInterceptorService', function($q, $injector,
+    localStorage) {
+    var service = {};
+    /**
+     * function that intercept all request and adds token if its present
+     * @param  {Object} config configuration object
+     * @return {Object}        modified configuration object
+     */
+    service.request = function(config) {
+      config.headers = config.headers || {};
+      var authData = localStorage.get('authData');
+      if (authData) {
+        config.headers.Authorization = 'Bearer ' + authData.token;
+      }
+      return config;
+    };
+    /**
+     * Function will executes when their is some error with the response if response is Unauthorized,Forbidden or Not Acceptable
+     * @param  {Object} response response object which contains response error and response status
+     * @return {Promise}          Promise
+     */
+    service.responseError = function(response) {
+      var $state = $injector.get('$state');
+      if (response.status === 401) {
+        $state.go('login');
+      }
+      return $q.reject(response);
+    };
+    return service;
+  });
+})(angular.module('app.auth'));
